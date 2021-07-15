8ddcbc744bbaa812368ab0937322644a054562af
James <50501825+Gobot1234@users.noreply.github.com>
2021-07-15 11:56:08 +0100

    Update client.py

--- discord/ext/oauth/client.py
+++ discord/ext/oauth/client.py
@@ -1,130 +1,47 @@
-import weakref
+import aiohttp
 
-from typing import Optional, Union, List
-
-from .http import Route, HTTPClient
-from .token import AccessTokenResponse
-from .user import User
+from .errors import HTTPException
 
 
 __all__: tuple = (
-    "OAuth2Client",
+    "Route", 
+    "HTTPClient"
 )
 
 
-class OAuth2Client:
-    """
-    A class representing a client interacting with the discord OAuth2 API.
-    """
-    def __init__(
-        self,
-        *,
-        client_id: int,
-        client_secret: str,
-        redirect_uri: str,
-        scopes: Optional[List[str]] = None
-    ):
-        """A class representing a client interacting with the discord OAuth2 API.
+class Route:
+    BASE = "https://discord.com/api/v9"
 
-        :param client_id: The OAuth application's client_id
-        :type client_id: int
-        :param client_secret: The OAuth application's client_secret
-        :type client_secret: str
-        :param redirect_uri: The OAuth application's redirect_uri. Must be from one of the configured uri's on the developer portal
-        :type redirect_uri: str
-        :param scopes: A list of OAuth2 scopes, defaults to None
-        :type scopes: Optional[List[str]], optional
-        """
-        self._id = client_id
-        self._auth = client_secret
-        self._redirect = redirect_uri
-        self._scopes = " ".join(scopes) if scopes is not None else None
+    def __init__(self, method: str, endpoint: str, **params):
+        self.url = self.BASE + endpoint.format(**params)
+        self.method = method
 
-        self.http = HTTPClient()
-        self.http._state_info.update(
-            {
-                "client_id": self._id,
-                "client_secret": self._auth,
-                "redirect_uri": self._redirect,
-                "scopes": self._scopes,
-            }
-        )
 
-        self._user_cache = weakref.WeakValueDictionary()
+class HTTPClient:
+    def __init__(self):
+        self.__session = None  # filled in later
+        self._state_info = {}  # client fills this
 
-    async def exchange_code(self, code: str) -> AccessTokenResponse:
-        """Exchanges the code you receive from the OAuth2 redirect.
+    async def _create_session(self) -> aiohttp.ClientSession:
+        self.__session = aiohttp.ClientSession()
+        return self.__session
 
-        :param code: The code you've received from the OAuth2 redirect
-        :type code: str
-        :return: A response class containing information about the access token
-        :rtype: AccessTokenResponse
-        """
-        route = Route("POST", "/oauth2/token")
-        post_data = {
-            "client_id": self._id,
-            "client_secret": self._auth,
-            "grant_type": "authorization_code",
-            "code": code,
-            "redirect_uri": self._redirect,
-        }
-        if self._scopes is not None:
-            post_data["scope"] = self._scopes
-        request_data = await self.http.request(route, data=post_data)
-        token_resp = AccessTokenResponse(data=request_data)
-        return token_resp
+    async def request(self, route: Route, **kwargs) -> dict:
+        if self.__session is None or self.__session.closed is True:
+            await self._create_session()
 
-    async def refresh_token(self, refresh_token: Union[str, AccessTokenResponse]) -> AccessTokenResponse:
-        """Refreshes an access token. Takes either a string or an AccessTokenResponse.
+        headers = kwargs.pop("headers", {})
 
-        :param refresh_token: The refresh token you received when exchanging a redirect code
-        :type refresh_token: Union[str, AccessTokenResponse]
-        :return: A new access token response containg information about the refreshed access token
-        :rtype: AccessTokenResponse
-        """
-        refresh_token = (
-            refresh_token if isinstance(refresh_token, str) else refresh_token.token
-        )
-        route = Route("POST", "/oauth2/token")
-        post_data = {
-            "client_id": self._id,
-            "client_secret": self._auth,
-            "grant_type": "refresh_token",
-            "refresh_token": refresh_token,
-        }
-        request_data = await self.http.request(route, data=post_data)
-        token_resp = AccessTokenResponse(data=request_data)
-        return token_resp
+        headers["Content-Type"] = "application/x-www-form-urlencoded"  # the discord OAuth2 api requires this header to be set to this
+        kwargs["headers"] = headers
 
-    async def fetch_user(self, access_token_response: AccessTokenResponse) -> User:
-        """Makes an api call to fetch a user using their access token.
-
-        :param access_token_response: A class holding information about an access token
-        :type access_token_response: AccessTokenResponse
-        :return: Returns a User object holding information about the select user
-        :rtype: User
-        """
-        access_token = access_token_response.token
-        route = Route("GET", "/users/@me")
-        headers = {"Authorization": "Bearer {}".format(access_token)}
-        resp = await self.http.request(route, headers=headers)
-        user = User(http=self.http, data=resp, acr=access_token_response)
-        self._user_cache.update({user.id: user})
-        return user
-
-    def get_user(self, id: int) -> Optional[User]:
-        """Gets a user from the cache. The cache is a WeakValueDictionary, so objects may be removed without notice.
-
-        :param id: The id of the user you want to get
-        :type id: int
-        :return: A possible user object. Returns None if no User is found in cache.
-        :rtype: Optional[User]
-        """
-        user = self._user_cache.get(id)
-        return user
+        async with self.__session.request(route.method, route.url, **kwargs) as resp:
+            json = await resp.json()
+            if 200 <= resp.status < 300:
+                return json
+            else:
+                raise HTTPException(resp, json=json)
 
     async def close(self):
-        """Closes and performs cleanup operations on the client, such as clearing its cache.
-        """
-        self._user_cache.clear()
-        await self.http.close()
+        await self.__session.close()
+        self.__session = None
